4b0bc189d57753809bf9cc43d0843c1b9ea4112a
Imesh Chandula <117582263+ImeshChandula@users.noreply.github.com>
2025-04-03 13:43:45 +0530

    test complete

--- newspaper-backend/routes/userRoutes.js
+++ newspaper-backend/routes/userRoutes.js
@@ -18,24 +18,19 @@ const router = express.Router();
 
 router.post("/register", registerUser);
 router.post("/login", loginUser);
-router.get("/getAllUsers", getAllUsers);
-router.get("/getAllEditors", getAllEditors);
+
+router.get("/getAllUsers", authenticateUser, authorizeRoles("super_admin"), getAllUsers);
+router.get("/getAllEditors", authenticateUser, authorizeRoles("admin", "super_admin"), getAllEditors);
 router.get("/getAllAdmins", authenticateUser, authorizeRoles("super_admin"), getAllAdmins);
 router.get("/getAllSuperAdmins", authenticateUser, authorizeRoles("super_admin"), getAllSuperAdmins);
-router.patch("/updateUser:id", authenticateUser, authorizeRoles("super_admin"), updateUser);
+
 router.delete("/deleteUser:name", authenticateUser, authorizeRoles("super_admin"), deleteUser);
+router.patch("/updateUser:id", authenticateUser, authorizeRoles("super_admin"), updateUser);
 router.patch("/promoteToAdmin:username", authenticateUser, authorizeRoles("super_admin"), promoteToAdmin);
 router.patch("/promoteToSuperAdmin:username", authenticateUser, authorizeRoles("super_admin"), promoteToSuperAdmin);
 
 
-//router.get("/getAllUsers", authenticateUser, authorizeRoles("admin", "super_admin"), getAllUsers);
-//router.get("/getAllEditors", authenticateUser, authorizeRoles("admin", "super_admin"), getAllEditors);
-//router.get("/getAllAdmins", authenticateUser, authorizeRoles("admin", "super_admin"), getAllAdmins);
-//router.get("/getAllSuperAdmins", authenticateUser, authorizeRoles("super_admin"), getAllSuperAdmins);
-//router.put("/updateUser:id", authenticateUser, authorizeRoles("super_admin"), updateUser);
-//router.delete("/deleteUser:name", authenticateUser, authorizeRoles("super_admin"), deleteUser);
-//router.patch("/promoteToAdmin:username", authenticateUser, authorizeRoles("super_admin"), promoteToAdmin);
-//router.patch("/promoteToSuperAdmin:username", authenticateUser, authorizeRoles("super_admin"), promoteToSuperAdmin);
+
 
 
 module.exports = router;
