d1884615968930ce351158047f82cb5ce3d4eb2e
Pasindu Hirumal <pasinduhirumal7@gmail.com>
2025-04-07 17:11:27 +0530

    responsive nav bar

--- front-end/src/components/Navbar.jsx
+++ front-end/src/components/Navbar.jsx
@@ -63,21 +63,24 @@ const Navbar = () => {
           type="button"
           data-bs-toggle="collapse"
           data-bs-target="#navbarNav"
+          aria-controls="navbarNav"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
         >
           <span className="navbar-toggler-icon"></span>
         </button>
 
-        <div className="collapse navbar-collapse mt-3 mt-lg-0" id="navbarNav">
+        {/* Collapsible Menu with Scrollable Property */}
+        <div className="collapse navbar-collapse overflow-auto mt-3 mt-lg-0" id="navbarNav">
           <ul className="navbar-nav ms-auto align-items-lg-center gap-2">
-
             <div className="nav-item d-lg-none w-100 mb-3">
               <ProfilForSD />
             </div>
 
-            <li className="nav-item"><Link className="nav-link px-3" to="/">Home</Link></li>
-            <li className="nav-item"><Link className="nav-link px-3" to="/sport">Sport</Link></li>
-            <li className="nav-item"><Link className="nav-link px-3" to="/education">Education</Link></li>
-            <li className="nav-item"><Link className="nav-link px-3" to="/politics">Politics</Link></li>
+            <li className="nav-item"><Link className="nav-link" to="/">Home</Link></li>
+            <li className="nav-item"><Link className="nav-link" to="/sport">Sport</Link></li>
+            <li className="nav-item"><Link className="nav-link" to="/education">Education</Link></li>
+            <li className="nav-item"><Link className="nav-link" to="/politics">Politics</Link></li>
 
             <li className="nav-item d-lg-none w-100 mt-2">
               <form className="d-flex" onSubmit={handleSearch}>
@@ -94,8 +97,8 @@ const Navbar = () => {
           </ul>
         </div>
 
+        {/* Right Section for Larger Screens */}
         <div className="d-none d-lg-flex align-items-center ms-auto gap-3">
-
           <form className="d-flex me-2" onSubmit={handleSearch}>
             <input
               className="form-control me-2"
