66a37f261bc610dfe6c5ca74e0acf8da39a3a3f7
Imesh Chandula <117582263+ImeshChandula@users.noreply.github.com>
2025-04-07 13:06:52 +0530

    auth context

--- front-end/src/context/AuthContext.js
+++ front-end/src/context/AuthContext.js
@@ -1,35 +1,46 @@
 import { createContext, useState, useEffect } from "react";
 import axios from "axios";
 import { jwtDecode } from "jwt-decode";
+import { useNavigate } from "react-router-dom";
 
 export const AuthContext = createContext();
 
 export const AuthProvider = ({ children }) => {
   const [user, setUser] = useState(null);
+  const navigate = useNavigate();
 
   useEffect(() => {
     const token = localStorage.getItem("token");
     if (token) {
       const decoded = jwtDecode(token);
-      setUser(decoded);
+      if (decoded.exp * 1000 > Date.now()) { // Check if token is still valid
+        setUser(decoded);
+      } else {
+        logout(); // If expired, clear token and logout
+      }
     }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
   const login = async (credentials) => {
     try {
       const res = await axios.post(`${process.env.REACT_APP_API_BASE_URL_USERS}/login`, credentials);
       localStorage.setItem("token", res.data.token);
-      setUser(jwtDecode(res.data.token));
-      return true;// Indicate successful login
+
+      const decodedUser = jwtDecode(res.data.token);
+      setUser(decodedUser);
+
+      return { success: true, role: decodedUser.role  };
     } catch (error) {
       console.error("Login failed", error);
-      return false; // Indicate failed login
+      return { success: false, message: error.response?.data?.message || "Login failed" };
     }
   };
 
   const logout = () => {
     localStorage.removeItem("token");
     setUser(null);
+    navigate("/");
   };
 
   return (
