57fe939f6410d7286c627a2a211a063647dbcb67
Imesh Chandula <117582263+ImeshChandula@users.noreply.github.com>
2025-04-07 15:22:13 +0530

    unauthorized page done

--- front-end/src/routes/Unauthorized.jsx
+++ front-end/src/routes/Unauthorized.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
+import '../components/css/Unauthorized.css';
+
 
 const Unauthorized = () => {
   return (
-    <div className="flex flex-col items-center justify-center h-screen">
-      <h1 className="text-3xl font-bold text-red-500">Access Denied</h1>
-      <p className="text-lg mt-2">You do not have permission to access this page.</p>
-      <a href="/login" className="text-blue-500 mt-4">Go to Login</a>
+    <div className="unauthorized">
+      <h1 className="unauthorized_heading">Access Denied</h1>
+      <p className="unauthorized_message">You do not have permission to access this page.</p>
+      <a href="/login" className="unauthorized_text">Go to Login</a>
     </div>
   )
 }
