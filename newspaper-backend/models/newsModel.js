00d086914c874ef1d1fee939b4ff76cf9b01f922
Imesh Chandula <117582263+ImeshChandula@users.noreply.github.com>
2025-04-08 09:27:58 +0530

    update news model

--- newspaper-backend/models/newsModel.js
+++ newspaper-backend/models/newsModel.js
@@ -1,11 +1,21 @@
 const mongoose = require('mongoose');
 
 const newsSchema = new mongoose.Schema({
+    category: {
+        type: String,
+        enum: ["Education", "Politics", "Sports"],
+        required: true,
+      },
     title: { type: String, required: true },
     media: { type: String, required: false }, // URL for image or video
     content: { type: String, required: true },
     date: { type: Date, default: Date.now },
-    author: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true }
+    author: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
+    status: {
+        type: String,
+        enum: ["accept", "pending", "reject"],
+        default: "pending",
+    },
 });
 
 const News = mongoose.model('News', newsSchema);
