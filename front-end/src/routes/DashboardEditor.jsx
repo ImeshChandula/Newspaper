14aaeb1d8ffdbfc6cc7d5c917390d0d53b4f87b4
Imesh Chandula <117582263+ImeshChandula@users.noreply.github.com>
2025-04-10 14:25:47 +0530

    update styling

--- front-end/src/routes/DashboardEditor.jsx
+++ front-end/src/routes/DashboardEditor.jsx
@@ -1,9 +1,13 @@
 import React from 'react';
-import CreateNewsArticle from "../components/CreateNewsArticle"
+import CreateNewsArticle from "../components/CreateNewsArticle";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import '../components/css/DashboardSuperAdmin.css';
 
 const DashboardEditor = () => {
   return (
-    <div>
+    <div className="editor-dashboard"> 
+      <h1 className="dashboard-title">Author Dashboard</h1>
+      
       <CreateNewsArticle />
     </div>
   )
