a6cdec64182f0bff3b5dc2f23292ec6f9b7b1aa4
Imesh Chandula <117582263+ImeshChandula@users.noreply.github.com>
2025-05-06 19:59:38 +0530

    modify

--- back-end/server.js
+++ back-end/server.js
@@ -12,7 +12,7 @@ connectDB();
 
 const app = express();
 app.use(express.json());
-app.use(cors({ origin: "https://sivdesanews.lk", credentials: true }));
+app.use(cors({ origin: process.env.FRONTEND_URL, credentials: true }));
 app.use(cookieParser());
 
 
