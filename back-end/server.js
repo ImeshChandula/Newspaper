b3573bed975170015f1870bce3d2d157dbd4e7dc
Imesh Chandula <117582263+ImeshChandula@users.noreply.github.com>
2025-05-06 09:19:39 +0530

    remove super admin

--- back-end/server.js
+++ back-end/server.js
@@ -9,42 +9,9 @@ const bcrypt = require("bcryptjs");
 dotenv.config();
 connectDB();
 
-// Create a default Super Admin
-const createDefaultSuperAdmin = async () => {
-  try {
-      const existingSuperAdmin = await User.findOne({ username: "superAdmin" });
-      if (!existingSuperAdmin) {
-        const defaultUsername = "superAdmin";
-        const defaultPassword = "super123";
-        const defaultRole = "super_admin";
-
-        const salt = await bcrypt.genSalt(10);
-        const hashedPassword = await bcrypt.hash(defaultPassword, salt);
-
-          const defaultSuperAdmin = new User({
-              username: defaultUsername,
-              password: hashedPassword, 
-              role: defaultRole,
-          });
-          await defaultSuperAdmin.save();
-          console.log("Default Super Admin created: superAdmin / super123");
-      } else {
-          console.log("Default Super Admin already exists. superAdmin / super123");
-      }
-  } catch (error) {
-      console.error("Error creating default Super Admin:", error);
-  }
-};
-
-
-
 
 const app = express();
 app.use(express.json());
-
-// Run this function on server start
-createDefaultSuperAdmin();
-
 app.use(cors({ origin: "http://localhost:3000", credentials: true }));
 app.use(cookieParser());
 
